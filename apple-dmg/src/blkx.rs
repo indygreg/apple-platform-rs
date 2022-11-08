0fc53447a467deedd544e671f8bddf7c1ec4112e
David Craven <david@craven.ch>
2022-11-08 06:15:15 +0100

    apple-dmg: add project (#39)

---
+++ apple-dmg/src/blkx.rs
@@ -0,0 +1,222 @@
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+use crate::koly::UdifChecksum;
+use anyhow::Result;
+use byteorder::{ReadBytesExt, WriteBytesExt, BE};
+use std::io::{Read, Write};
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct BlkxTable {
+    /// currently 1
+    pub version: u32,
+    /// starting sector
+    pub sector_number: u64,
+    /// number of sectors
+    pub sector_count: u64,
+    /// seems to always be 0
+    pub data_offset: u64,
+    /// seems to be a magic constant for zlib describing the buffer size
+    /// required for decompressing a chunk.
+    pub buffers_needed: u32,
+    /// not sure what this is, setting it to the partition index
+    pub block_descriptors: u32,
+    pub reserved: [u8; 24],
+    pub checksum: UdifChecksum,
+    /// chunk table
+    pub chunks: Vec<BlkxChunk>,
+}
+
+impl Default for BlkxTable {
+    fn default() -> Self {
+        Self {
+            version: 1,
+            sector_number: 0,
+            sector_count: 0,
+            data_offset: 0,
+            //  number was taken from hdiutil
+            buffers_needed: 2056,
+            block_descriptors: 0,
+            reserved: [0; 24],
+            checksum: UdifChecksum::default(),
+            chunks: vec![],
+        }
+    }
+}
+
+impl BlkxTable {
+    pub fn new(index: u32, sector: u64, checksum: u32) -> Self {
+        Self {
+            block_descriptors: index,
+            sector_number: sector,
+            checksum: UdifChecksum::new(checksum),
+            ..Default::default()
+        }
+    }
+
+    pub fn add_chunk(&mut self, mut chunk: BlkxChunk) {
+        chunk.sector_number = self.sector_count;
+        self.sector_count += chunk.sector_count;
+        self.chunks.push(chunk);
+    }
+
+    pub fn read_from<R: Read>(r: &mut R) -> Result<Self> {
+        let mut signature = [0; 4];
+        r.read_exact(&mut signature)?;
+        anyhow::ensure!(&signature == b"mish");
+        let version = r.read_u32::<BE>()?;
+        let sector_number = r.read_u64::<BE>()?;
+        let sector_count = r.read_u64::<BE>()?;
+        let data_offset = r.read_u64::<BE>()?;
+        let buffers_needed = r.read_u32::<BE>()?;
+        let block_descriptors = r.read_u32::<BE>()?;
+        let mut reserved = [0; 24];
+        r.read_exact(&mut reserved)?;
+        let checksum = UdifChecksum::read_from(r)?;
+        let num_chunks = r.read_u32::<BE>()?;
+        let mut chunks = Vec::with_capacity(num_chunks as _);
+        for _ in 0..num_chunks {
+            chunks.push(BlkxChunk::read_from(r)?);
+        }
+        Ok(Self {
+            version,
+            sector_number,
+            sector_count,
+            data_offset,
+            buffers_needed,
+            block_descriptors,
+            reserved,
+            checksum,
+            chunks,
+        })
+    }
+
+    pub fn write_to<W: Write>(&self, w: &mut W) -> Result<()> {
+        w.write_all(b"mish")?;
+        w.write_u32::<BE>(self.version)?;
+        w.write_u64::<BE>(self.sector_number)?;
+        w.write_u64::<BE>(self.sector_count)?;
+        w.write_u64::<BE>(self.data_offset)?;
+        w.write_u32::<BE>(self.buffers_needed)?;
+        w.write_u32::<BE>(self.block_descriptors)?;
+        w.write_all(&self.reserved)?;
+        self.checksum.write_to(w)?;
+        w.write_u32::<BE>(self.chunks.len() as u32)?;
+        for chunk in &self.chunks {
+            chunk.write_to(w)?;
+        }
+        Ok(())
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub struct BlkxChunk {
+    /// compression type used for this chunk
+    pub r#type: u32,
+    pub comment: u32,
+    pub sector_number: u64,
+    pub sector_count: u64,
+    pub compressed_offset: u64,
+    pub compressed_length: u64,
+}
+
+impl Default for BlkxChunk {
+    fn default() -> Self {
+        Self {
+            r#type: ChunkType::Raw as _,
+            comment: 0,
+            sector_number: 0,
+            sector_count: 0,
+            compressed_offset: 0,
+            compressed_length: 0,
+        }
+    }
+}
+
+impl BlkxChunk {
+    pub fn new(
+        ty: ChunkType,
+        sector_number: u64,
+        sector_count: u64,
+        compressed_offset: u64,
+        compressed_length: u64,
+    ) -> Self {
+        Self {
+            r#type: ty as _,
+            sector_number,
+            sector_count,
+            compressed_offset,
+            compressed_length,
+            ..Default::default()
+        }
+    }
+
+    pub fn term(sector_number: u64, compressed_offset: u64) -> Self {
+        Self::new(ChunkType::Term, sector_number, 0, compressed_offset, 0)
+    }
+
+    pub fn read_from<R: Read>(r: &mut R) -> Result<Self> {
+        let r#type = r.read_u32::<BE>()?;
+        let comment = r.read_u32::<BE>()?;
+        let sector_number = r.read_u64::<BE>()?;
+        let sector_count = r.read_u64::<BE>()?;
+        let compressed_offset = r.read_u64::<BE>()?;
+        let compressed_length = r.read_u64::<BE>()?;
+        Ok(Self {
+            r#type,
+            comment,
+            sector_number,
+            sector_count,
+            compressed_offset,
+            compressed_length,
+        })
+    }
+
+    pub fn write_to<W: Write>(&self, w: &mut W) -> Result<()> {
+        w.write_u32::<BE>(self.r#type)?;
+        w.write_u32::<BE>(self.comment)?;
+        w.write_u64::<BE>(self.sector_number)?;
+        w.write_u64::<BE>(self.sector_count)?;
+        w.write_u64::<BE>(self.compressed_offset)?;
+        w.write_u64::<BE>(self.compressed_length)?;
+        Ok(())
+    }
+
+    pub fn ty(self) -> Option<ChunkType> {
+        ChunkType::from_u32(self.r#type)
+    }
+}
+
+/// Possible compression types of the BlkxChunk.
+#[repr(u32)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum ChunkType {
+    Zero = 0x0000_0000,
+    Raw = 0x0000_0001,
+    Ignore = 0x0000_0002,
+    Comment = 0x7fff_fffe,
+    Adc = 0x8000_0004,
+    Zlib = 0x8000_0005,
+    Bzlib = 0x8000_0006,
+    Lzfse = 0x8000_0007,
+    Term = 0xffff_ffff,
+}
+
+impl ChunkType {
+    pub fn from_u32(ty: u32) -> Option<Self> {
+        Some(match ty {
+            x if x == ChunkType::Zero as u32 => ChunkType::Zero,
+            x if x == ChunkType::Raw as u32 => ChunkType::Raw,
+            x if x == ChunkType::Ignore as u32 => ChunkType::Ignore,
+            x if x == ChunkType::Comment as u32 => ChunkType::Comment,
+            x if x == ChunkType::Adc as u32 => ChunkType::Adc,
+            x if x == ChunkType::Zlib as u32 => ChunkType::Zlib,
+            x if x == ChunkType::Bzlib as u32 => ChunkType::Bzlib,
+            x if x == ChunkType::Lzfse as u32 => ChunkType::Lzfse,
+            x if x == ChunkType::Term as u32 => ChunkType::Term,
+            _ => return None,
+        })
+    }
+}
---
+++ apple-dmg/src/lib.rs
@@ -0,0 +1,353 @@
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+use anyhow::Result;
+use crc32fast::Hasher;
+use fatfs::{Dir, FileSystem, FormatVolumeOptions, FsOptions, ReadWriteSeek};
+use flate2::bufread::ZlibEncoder;
+use flate2::read::ZlibDecoder;
+use flate2::Compression;
+use fscommon::BufStream;
+use gpt::mbr::{PartRecord, ProtectiveMBR};
+use std::fs::File;
+use std::io::{BufReader, BufWriter, Cursor, Read, Seek, SeekFrom, Write};
+use std::path::Path;
+
+mod blkx;
+mod koly;
+mod xml;
+
+pub use crate::blkx::*;
+pub use crate::koly::*;
+pub use crate::xml::*;
+
+pub struct DmgReader<R: Read + Seek> {
+    koly: KolyTrailer,
+    xml: Plist,
+    r: R,
+}
+
+impl DmgReader<BufReader<File>> {
+    pub fn open(path: &Path) -> Result<Self> {
+        let r = BufReader::new(File::open(path)?);
+        Self::new(r)
+    }
+}
+
+impl<R: Read + Seek> DmgReader<R> {
+    pub fn new(mut r: R) -> Result<Self> {
+        let koly = KolyTrailer::read_from(&mut r)?;
+        r.seek(SeekFrom::Start(koly.plist_offset))?;
+        let mut xml = Vec::with_capacity(koly.plist_length as usize);
+        (&mut r).take(koly.plist_length).read_to_end(&mut xml)?;
+        let xml: Plist = plist::from_reader_xml(&xml[..])?;
+        Ok(Self { koly, xml, r })
+    }
+
+    pub fn koly(&self) -> &KolyTrailer {
+        &self.koly
+    }
+
+    pub fn plist(&self) -> &Plist {
+        &self.xml
+    }
+
+    pub fn sector(&mut self, chunk: &BlkxChunk) -> Result<impl Read + '_> {
+        self.r.seek(SeekFrom::Start(chunk.compressed_offset))?;
+        let compressed_chunk = (&mut self.r).take(chunk.compressed_length);
+        match chunk.ty().expect("unknown chunk type") {
+            ChunkType::Ignore | ChunkType::Zero | ChunkType::Comment => {
+                Ok(Box::new(std::io::repeat(0).take(chunk.compressed_length)) as Box<dyn Read>)
+            }
+            ChunkType::Raw => Ok(Box::new(compressed_chunk)),
+            ChunkType::Zlib => Ok(Box::new(ZlibDecoder::new(compressed_chunk))),
+            ChunkType::Adc | ChunkType::Bzlib | ChunkType::Lzfse => unimplemented!(),
+            ChunkType::Term => Ok(Box::new(std::io::empty())),
+        }
+    }
+
+    pub fn data_checksum(&mut self) -> Result<u32> {
+        self.r.seek(SeekFrom::Start(self.koly.data_fork_offset))?;
+        let mut data_fork = Vec::with_capacity(self.koly.data_fork_length as usize);
+        (&mut self.r)
+            .take(self.koly.data_fork_length)
+            .read_to_end(&mut data_fork)?;
+        Ok(crc32fast::hash(&data_fork))
+    }
+
+    pub fn partition_table(&self, i: usize) -> Result<BlkxTable> {
+        self.plist().partitions()[i].table()
+    }
+
+    pub fn partition_name(&self, i: usize) -> &str {
+        &self.plist().partitions()[i].name
+    }
+
+    pub fn partition_data(&mut self, i: usize) -> Result<Vec<u8>> {
+        let table = self.plist().partitions()[i].table()?;
+        let mut partition = vec![];
+        for chunk in &table.chunks {
+            std::io::copy(&mut self.sector(chunk)?, &mut partition)?;
+        }
+        Ok(partition)
+    }
+}
+
+pub struct DmgWriter<W: Write + Seek> {
+    xml: Plist,
+    w: W,
+    data_hasher: Hasher,
+    main_hasher: Hasher,
+    sector_number: u64,
+    compressed_offset: u64,
+}
+
+impl DmgWriter<BufWriter<File>> {
+    pub fn create(path: &Path) -> Result<Self> {
+        let w = BufWriter::new(File::create(path)?);
+        Ok(Self::new(w))
+    }
+}
+
+impl<W: Write + Seek> DmgWriter<W> {
+    pub fn new(w: W) -> Self {
+        Self {
+            xml: Default::default(),
+            w,
+            data_hasher: Hasher::new(),
+            main_hasher: Hasher::new(),
+            sector_number: 0,
+            compressed_offset: 0,
+        }
+    }
+
+    pub fn create_fat32(mut self, fat32: &[u8]) -> Result<()> {
+        anyhow::ensure!(fat32.len() % 512 == 0);
+        let sector_count = fat32.len() as u64 / 512;
+        let mut mbr = ProtectiveMBR::new();
+        let mut partition = PartRecord::new_protective(Some(sector_count.try_into()?));
+        partition.os_type = 11;
+        mbr.set_partition(0, partition);
+        let mbr = mbr.as_bytes()?;
+        self.add_partition("Master Boot Record (MBR : 0)", &mbr)?;
+        self.add_partition("FAT32 (FAT32 : 1)", fat32)?;
+        self.finish()?;
+        Ok(())
+    }
+
+    pub fn add_partition(&mut self, name: &str, bytes: &[u8]) -> Result<()> {
+        anyhow::ensure!(bytes.len() % 512 == 0);
+        let id = self.xml.partitions().len() as u32;
+        let name = name.to_string();
+        let mut table = BlkxTable::new(id, self.sector_number, crc32fast::hash(bytes));
+        for chunk in bytes.chunks(2048 * 512) {
+            let mut encoder = ZlibEncoder::new(chunk, Compression::best());
+            let mut compressed = vec![];
+            encoder.read_to_end(&mut compressed)?;
+            let compressed_length = compressed.len() as u64;
+            let sector_count = chunk.len() as u64 / 512;
+            self.w.write_all(&compressed)?;
+            self.data_hasher.update(&compressed);
+            table.add_chunk(BlkxChunk::new(
+                ChunkType::Zlib,
+                self.sector_number,
+                sector_count,
+                self.compressed_offset,
+                compressed_length,
+            ));
+            self.sector_number += sector_count;
+            self.compressed_offset += compressed_length;
+        }
+        table.add_chunk(BlkxChunk::term(self.sector_number, self.compressed_offset));
+        self.main_hasher.update(&table.checksum.data[..4]);
+        self.xml
+            .add_partition(Partition::new(id as i32 - 1, name, table));
+        Ok(())
+    }
+
+    pub fn finish(mut self) -> Result<()> {
+        let mut xml = vec![];
+        plist::to_writer_xml(&mut xml, &self.xml)?;
+        let pos = self.w.seek(SeekFrom::Current(0))?;
+        let data_digest = self.data_hasher.finalize();
+        let main_digest = self.main_hasher.finalize();
+        let koly = KolyTrailer::new(
+            pos,
+            self.sector_number,
+            pos,
+            xml.len() as _,
+            data_digest,
+            main_digest,
+        );
+        self.w.write_all(&xml)?;
+        koly.write_to(&mut self.w)?;
+        Ok(())
+    }
+}
+
+// https://wiki.samba.org/index.php/UNIX_Extensions#Storing_symlinks_on_Windows_servers
+fn symlink(target: &str) -> Result<Vec<u8>> {
+    let xsym = format!(
+        "XSym\n{:04}\n{:x}\n{}\n",
+        target.as_bytes().len(),
+        md5::compute(target.as_bytes()),
+        target,
+    );
+    let mut xsym = xsym.into_bytes();
+    anyhow::ensure!(xsym.len() <= 1067);
+    xsym.resize(1067, b' ');
+    Ok(xsym)
+}
+
+fn add_dir<T: ReadWriteSeek>(src: &Path, dest: &Dir<'_, T>) -> Result<()> {
+    for entry in std::fs::read_dir(src)? {
+        let entry = entry?;
+        let file_name = entry.file_name();
+        let file_name = file_name.to_str().unwrap();
+        let source = src.join(file_name);
+        let file_type = entry.file_type()?;
+        if file_type.is_dir() {
+            let d = dest.create_dir(file_name)?;
+            add_dir(&source, &d)?;
+        } else if file_type.is_file() {
+            let mut f = dest.create_file(file_name)?;
+            std::io::copy(&mut File::open(source)?, &mut f)?;
+        } else if file_type.is_symlink() {
+            let target = std::fs::read_link(&source)?;
+            let xsym = symlink(target.to_str().unwrap())?;
+            let mut f = dest.create_file(file_name)?;
+            std::io::copy(&mut &xsym[..], &mut f)?;
+        }
+    }
+    Ok(())
+}
+
+pub fn create_dmg(dir: &Path, dmg: &Path, volume_label: &str, total_sectors: u32) -> Result<()> {
+    let mut fat32 = vec![0; total_sectors as usize * 512];
+    {
+        let mut volume_label_bytes = [0; 11];
+        let end = std::cmp::min(volume_label_bytes.len(), volume_label.len());
+        volume_label_bytes[..end].copy_from_slice(&volume_label.as_bytes()[..end]);
+        let volume_options = FormatVolumeOptions::new()
+            .volume_label(volume_label_bytes)
+            .bytes_per_sector(512)
+            .total_sectors(total_sectors);
+        let mut disk = BufStream::new(Cursor::new(&mut fat32));
+        fatfs::format_volume(&mut disk, volume_options)?;
+        let fs = FileSystem::new(disk, FsOptions::new())?;
+        let file_name = dir.file_name().unwrap().to_str().unwrap();
+        let dest = fs.root_dir().create_dir(file_name)?;
+        add_dir(dir, &dest)?;
+    }
+    DmgWriter::create(dmg)?.create_fat32(&fat32)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use gpt::disk::LogicalBlockSize;
+
+    static DMG: &[u8] = include_bytes!("../assets/example.dmg");
+
+    fn print_dmg<R: Read + Seek>(dmg: &DmgReader<R>) -> Result<()> {
+        println!("{:?}", dmg.koly());
+        println!("{:?}", dmg.plist());
+        for partition in dmg.plist().partitions() {
+            let table = partition.table()?;
+            println!("{:?}", table);
+            println!("table checksum 0x{:x}", u32::from(table.checksum));
+            for (i, chunk) in table.chunks.iter().enumerate() {
+                println!("{} {:?}", i, chunk);
+            }
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn read_koly_trailer() -> Result<()> {
+        let koly = KolyTrailer::read_from(&mut Cursor::new(DMG))?;
+        //println!("{:#?}", koly);
+        let mut bytes = [0; 512];
+        koly.write_to(&mut &mut bytes[..])?;
+        let koly2 = KolyTrailer::read_from(&mut Cursor::new(&bytes))?;
+        assert_eq!(koly, koly2);
+        Ok(())
+    }
+
+    #[test]
+    fn only_read_dmg() -> Result<()> {
+        let mut dmg = DmgReader::new(Cursor::new(DMG))?;
+        print_dmg(&dmg)?;
+        assert_eq!(
+            UdifChecksum::new(dmg.data_checksum()?),
+            dmg.koly().data_fork_digest
+        );
+        let mut buffer = vec![];
+        let mut dmg2 = DmgWriter::new(Cursor::new(&mut buffer));
+        for i in 0..dmg.plist().partitions().len() {
+            let data = dmg.partition_data(i)?;
+            let name = dmg.partition_name(i);
+            dmg2.add_partition(name, &data)?;
+        }
+        dmg2.finish()?;
+        let mut dmg2 = DmgReader::new(Cursor::new(buffer))?;
+        print_dmg(&dmg2)?;
+        assert_eq!(
+            UdifChecksum::new(dmg2.data_checksum()?),
+            dmg2.koly().data_fork_digest
+        );
+        for i in 0..dmg.plist().partitions().len() {
+            let table = dmg.partition_table(i)?;
+            let data = dmg.partition_data(i)?;
+            let expected = u32::from(table.checksum);
+            let calculated = crc32fast::hash(&data);
+            assert_eq!(expected, calculated);
+        }
+        assert_eq!(dmg.koly().main_digest, dmg2.koly().main_digest);
+        println!("data crc32 0x{:x}", u32::from(dmg.koly().data_fork_digest));
+        println!("main crc32 0x{:x}", u32::from(dmg.koly().main_digest));
+        Ok(())
+    }
+
+    #[test]
+    fn read_dmg_partition_mbr() -> Result<()> {
+        let mut dmg = DmgReader::new(Cursor::new(DMG))?;
+        let mbr = dmg.partition_data(0)?;
+        println!("{:?}", mbr);
+        let mbr = ProtectiveMBR::from_bytes(&mbr, LogicalBlockSize::Lb512)?;
+        println!("{:?}", mbr);
+        Ok(())
+    }
+
+    #[test]
+    fn read_dmg_partition_fat32() -> Result<()> {
+        let mut dmg = DmgReader::new(Cursor::new(DMG))?;
+        let fat32 = dmg.partition_data(1)?;
+        let fs = FileSystem::new(Cursor::new(fat32), FsOptions::new())?;
+        println!("volume: {}", fs.volume_label());
+        for entry in fs.root_dir().iter() {
+            let entry = entry?;
+            println!("{}", entry.file_name());
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn checksum() -> Result<()> {
+        let mut dmg = DmgReader::new(Cursor::new(DMG))?;
+        assert_eq!(
+            UdifChecksum::new(dmg.data_checksum()?),
+            dmg.koly().data_fork_digest
+        );
+        for i in 0..dmg.plist().partitions().len() {
+            let table = dmg.partition_table(i)?;
+            let data = dmg.partition_data(i)?;
+            let expected = u32::from(table.checksum);
+            let calculated = crc32fast::hash(&data);
+            assert_eq!(expected, calculated);
+        }
+        Ok(())
+    }
+}
