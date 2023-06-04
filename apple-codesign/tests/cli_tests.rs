e5c37a91df2686d424dcc535739ddcdddf169144
Gregory Szorc <gregory.szorc@gmail.com>
2023-06-04 16:31:51 -0700

    apple-codesign: execute trycmd tests in a filesystem sandbox
    The upstream trycmd loader only supports filesystem sandboxes when
    `.out/` directories exist. I want to write tests that create files
    while only using `.trycmd` test files.
    
    Using the dynamic test loader functionality I added to trycmd, we
    modify the loading of `trycmd` files to always enable a filesystem
    sandbox.

--- apple-codesign/tests/cli_tests.rs
+++ apple-codesign/tests/cli_tests.rs
@@ -2,9 +2,32 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use {
+    std::path::Path,
+    trycmd::{schema::TryCmd, Error},
+};
+
+fn load_trycmd(path: &Path) -> Result<TryCmd, Error> {
+    let mut cmd = TryCmd::load_trycmd(path)?;
+
+    // CWD should be the crate root.
+    let cwd = std::env::current_dir().map_err(Error::new)?;
+
+    // We set the test to execute from a sandboxed copy of the crate root.
+    // This allows tests to create their own files without disturbing the
+    // source checkout.
+    cmd.fs.base = Some(cwd.clone());
+    cmd.fs.cwd = Some(cwd.clone());
+    cmd.fs.sandbox = Some(true);
+
+    Ok(cmd)
+}
+
 #[test]
 fn cli_tests() {
     let cases = trycmd::TestCases::new();
+
+    cases.file_extension_loader("trycmd", load_trycmd);
 
     cases.case("tests/cmd/*.trycmd").case("tests/cmd/*.toml");
 
